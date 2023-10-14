08c8e96d90bb745fe019aea43edc5050d021eedb
kumaran s <kumarandeveloper77@gmail.com>
2023-10-14 23:46:30 +0530

    new list added

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 var listItems = document.getElementsByClassName('list-group');
 listItems[2].style.background='green';
 listItems.style.fontWeight='bold';
+
+var listNew = document.getElementsByTagName('ul');
+var newList = listNew[4]
+
+
