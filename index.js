5621213b8eea9e7e5c90033f7589bbc25a2edd6e
kumaran s <kumarandeveloper77@gmail.com>
2023-10-15 01:54:31 +0530

    hello message added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var listItems = document.querySelectorAll('ul');
-listItems[1].style.background='green';
+
+/*listItems[1].style.background='green';
 listItems[2].style.display = "hidden";
 
 var odd = document.querySelectorAll('li:nth-child(odd)');
@@ -7,12 +7,18 @@ var odd = document.querySelectorAll('li:nth-child(odd)');
 for(var i=0; i<odd.length;i++){
     odd[i].style.background = "green";
 }
+*/
 
 
 
+var newDiv = document.getElementById('#header-title');
+
+var newDivText = document.createTextNode('Hello ');
+
+newDiv.appendChild(newDivText);
 
 
 
-
-
+var listItems = document.getElementById('#items');
+listItems[0].appendChild(newDivText);
 
