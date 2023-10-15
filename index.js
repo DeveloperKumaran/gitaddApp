e0bf945afcf25689f422d6b8fab5c29ddc62f2db
kumaran s <kumarandeveloper77@gmail.com>
2023-10-15 15:22:03 +0530

    newUser with object prperties

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ console.log('working');
 const nameValue = document.getElementById('fullName');
 const mailValue = document.getElementById('E-mail');
 
+class User{
+  name = ''
+  email = ''
 
+  constructor(name,email){
+     this.name = name
+     this.email = email
+  }
+}
 
 document.getElementById('Form').addEventListener('submit',(e)=>{
     
@@ -12,8 +20,16 @@ document.getElementById('Form').addEventListener('submit',(e)=>{
   
 const fullname = nameValue.value;
 const mail = mailValue.value;
-  localStorage.setItem('FullName',fullname);
-  localStorage.setItem('mail',mail);
+  /*localStorage.setItem('FullName',fullname);
+  localStorage.setItem('mail',mail);*/
+
+  let newUser = new User(fullname,mail);
+
+  let userString = JSON.stringify(newUser);
+
+  localStorage.setItem('userdetails',userString);
+
+
  
   console.log('submitted');
 });
